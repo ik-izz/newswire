1f8696dab2bc35108d1d85f11c4f7b61bfd28c73
ik-izz <94077710+ik-izz@users.noreply.github.com>
2022-07-12 16:26:17 -0400

    UserAuthentication

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,20 @@
 import axios from 'axios'
-import React, {useState} from 'react'
-import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+import React, { useState} from 'react'
+import { Link, useNavigate, useLocation, Navigate } from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
 
 const SignIn = () => {
+  const {setAuth} = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/homepage';
+
   const [formData, setFormData] = useState({
+    
     userName: '',
     password: '',
   })
-
-  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -18,13 +24,20 @@ const SignIn = () => {
       password: formData.password
       })
       .then((response) => {
-        console.log('user profile', response.data.user);
-        console.log( 'toke:' ,response.data.jwt);
-        console.log(response.data.user);
-        // <Routes>
-        // <Route path="/" element={response.data.user.confirmed ? <Navigate to='/homepage' /> : <p>login failed</p>} />
-        // </Routes>
-        response.data.user.confirmed && navigate('/homepage')
+        console.log('user profile', response?.data?.user);
+        console.log( 'token:' ,response?.data?.jwt);
+
+        const token = response?.data?.jwt;
+        //FIXME!!! need to set access based on roles
+        const roles = response?.data?.roles;
+
+        setAuth({ token, roles, status:true})
+
+        //upon successful login it navigates user to page they came from,
+        // if no path exists it will mavigate them to the homepage
+        navigate(from, {replace : true});
+        // response.data.user.confirmed && navigate('/homepage');
+
       })
     .catch((error) => {
       window.alert( error.response.data.error.message)
@@ -35,22 +48,22 @@ const SignIn = () => {
   return (
     <form onSubmit={handleSubmit}>
         <h1>Login Form</h1>
-        <label htmlFor='Name'>UserName</label>
+        <label htmlFor='username'>UserName:</label>
         <input 
           type='text' 
-          name='title'
+          id='username'
           placeholder='username'
-          autoComplete='new-username'
+          autoComplete='off'
           value={formData.title} 
           onChange={(e) => {setFormData({...formData, userName: e.target.value})}}
+          required
         />
 
-        <label htmlFor='Password'>Password</label>
+        <label htmlFor='password'>Password:</label>
         <input
           type='password'
-          name='password' 
+          id='password'
           placeholder='password'
-          autoComplete='new-password'
           value={formData.password} 
           onChange={(e) => {setFormData({...formData, password: e.target.value})}}
         ></input>
